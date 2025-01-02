059754ade4df7383cb5f8421cf06bf8ff5d9782e
Himanshuujha <himanshujha5288@gmail.com>
2025-01-02 17:43:14 +0530

    Add initial project structure with server and client setup, including environment configurations and Tailwind CSS integration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
-const ACTIONS = require('./src/Actions');
+const ACTIONS = require('../client/src/Actions');
 
 const server = http.createServer(app);
 const io = new Server(server);
